d6760272095f03b41bbd43133ae91e8519ab00d0
Scott Mitchell <mitchsco@sonoma.edu>
2018-12-30 20:12:57 -0800

    Forgot to revert changes

--- bot.py
+++ bot.py
@@ -217,7 +217,7 @@ class Spotty(commands.Bot):
 	async def on_ready(self):
 		logging.info('We have logged in as {0.user}'.format(self))
 		print('We have logged in as {0.user}'.format(self))
-		await discord_client.activity(activity=discord.Game(name="Spotify", type=discord.ActivityType.listening))
+		await discord_client.change_presence(activity=discord.Game(name="Spotify"))
 
 	async def on_message(self, message):
 		await self.process_commands(message)
